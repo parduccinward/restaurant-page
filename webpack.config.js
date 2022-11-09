7200341c2a1fc449264e201b1f8d874b87c273e2
Diego <dpardoblacutt@gmail.com>
2022-11-09 16:04:46 -0400

    Bundle files using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports ={
     entry: './src/index.js',
     output: {
-        filepath: 'main.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     }
 };
