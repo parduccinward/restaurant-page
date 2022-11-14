bc5126128b20823102e5200d77aaffd50117dc65
Diego <dpardoblacutt@gmail.com>
2022-11-14 19:11:57 -0400

    Change syntax from CommonJS to ESM

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-const path = require('path');
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-module.exports ={
+export default{
+    mode:'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-    }
+    },
+    devtool: 'inline-source-map',
 };
